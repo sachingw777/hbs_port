3f4030f8529317a28a7c81e1197c4ebc0b1780cf
Sachin Gwasikoti <sachingw777@gmail.com>
2020-09-22 12:24:31 +0530

    add helper

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ var createError = require('http-errors'),
     Comment = require('./models/comment'),
     Post = require('./models/post'),
     User = require('./models/user'),
-    exphbs = require('express-handlebars');
+    fs = require('fs-extra'),
+    multer = require('multer'),
+    Handlebars = require('handlebars'), 
+    expressHbs = require('express-handlebars');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,19 +23,35 @@ var postsRouter = require('./routes/posts');
 var commentsRouter = require('./routes/comments');
 
 var app = express();
-// mongoose.connect("mongodb://localhost/poster",{ useUnifiedTopology: true, useNewUrlParser: true }); 
-let url = process.env.DATABASEURL;
-mongoose.connect(url,{ useUnifiedTopology: true, useNewUrlParser: true });
-app.engine('ejs', engines.ejs);
+mongoose.connect("mongodb://localhost/poster",{ useUnifiedTopology: true, useNewUrlParser: true }); 
+// let url = process.env.DATABASEURL;
+// mongoose.connect(url,{ useUnifiedTopology: true, useNewUrlParser: true });
+// app.engine('ejs', engines.ejs);  //l1
 // app.engine('hbs', engines.hbs);
 // app.engine('html', engines.swig);
+app.engine("hbs", expressHbs({ extname: ".hbs",defaultLayout:false, viewsDir:__dirname + "/views", handlebars: allowInsecurePrototypeAccess(Handlebars) }));
+app.set("view engine", "hbs");
 
+var hbs = expressHbs.create({});
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// var storage = multer.diskStorage({ 
+//     destination: (req, file, cb) => { 
+//         cb(null, 'uploads') 
+//     }, 
+//     filename: (req, file, cb) => { 
+//         cb(null, file.fieldname + '-' + Date.now()) 
+//     } 
+// });
+// var upload = multer({ storage: storage });
+expressHbs.registerHelper('isEmpty', function(str,options) {
+    return (!str || 0 === str.length);
+});
 
 app.use(require("express-session")({
     secret: "bazooka",
@@ -71,7 +91,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-app.set('port', process.env.PORT || 3002);
+app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), process.env.IP, () => console.log("eai-c2 Server Has Started!"));
 
 module.exports = app;
